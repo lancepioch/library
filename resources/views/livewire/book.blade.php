9a156000a761b8da4bc03a71f1aba2769c2732a3
Lance Pioch <git@lance.sh>
2020-01-26 12:02:50 -0500

    Center the book images

--- resources/views/livewire/book.blade.php
+++ resources/views/livewire/book.blade.php
@@ -1,7 +1,11 @@
 
 <div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/5 rounded overflow-hidden shadow-md">
     <div>
-        <img class="h-40 px-6 py-2" src="https://books.google.com/books/content?id={{ $book['google_id'] ?? '' }}&printsec=frontcover&img=1&zoom=5" alt="">
+        <div class="justify-center flex">
+            <img class="h-40 py-2"
+                 src="https://books.google.com/books/content?id={{ $book['google_id'] ?? '' }}&printsec=frontcover&img=1&zoom=5"
+                 alt="{{ $book['title'] }}">
+        </div>
 
         @if (! $book['in_library'])
         <button wire:click="assignBook" class="m-4 ml-4 flex-shrink-0 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-2 rounded">Add to Library</button>
