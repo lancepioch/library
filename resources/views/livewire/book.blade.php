6cbc2adcb5d1505b62242da5067842553aa24c4a
Lance Pioch <git@lance.sh>
2020-01-26 12:07:18 -0500

    Make the list look nicer

--- resources/views/livewire/book.blade.php
+++ resources/views/livewire/book.blade.php
@@ -1,5 +1,5 @@
 
-<div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/5 rounded overflow-hidden shadow-md">
+<div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/5 rounded overflow-hidden shadow-md pt-3">
     <div>
         <div class="justify-center flex">
             <img class="h-40 py-2"
@@ -13,7 +13,7 @@
             <button wire:click="assignBook" class="m-4 ml-4 flex-shrink-0 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-2 rounded">Add to Library</button>
         @endif
 
-        <div class="px-6 py-4">
+        <div class="px-6">
             <div class="font-bold text-xl mb-2" title="{{ $book['title'] }}">{{ Str::limit($book['title'], 50) }}</div>
             <p class="text-gray-700 text-base">{{ Str::limit($book['description'] ?? '', 150) }}</p>
         </div>
