b1040a74297eed439d1a330030f09f0821b546e0
Lance Pioch <git@lance.sh>
2020-02-11 01:59:33 -0500

    Show library name and description

--- resources/views/livewire/library.blade.php
+++ resources/views/livewire/library.blade.php
@@ -1,6 +1,11 @@
 <div>
     <div class="container mx-auto flex w-full flex-wrap">
 
+        <div class="justify-content-center flex w-full">
+            <h2>{{ $library->name }}</h2>
+            <h3>{{ $library->description }}</h3>
+        </div>
+
         @forelse ($books as $book)
             @livewire('book', $book, $library, key($book->google_id))
         @empty
