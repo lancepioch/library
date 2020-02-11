8f78a856111c04af5f237c3c94d98c8a37745f84
Lance Pioch <git@lance.sh>
2020-02-11 01:57:14 -0500

    Use separate array

--- app/Http/Livewire/LibraryList.php
+++ app/Http/Livewire/LibraryList.php
@@ -8,24 +8,26 @@ use Livewire\Component;
 class LibraryList extends Component
 {
     public $library;
+    public $books;
 
     protected $listeners = ['echo:books,BookAdded' => 'notifyNewBook'];
 
     public function mount(Library $library)
     {
         $this->library = $library;
+        $this->books = $library->books;
     }
 
     public function notifyNewBook($parameters)
     {
-        
+        $book = $parameters['book'];
+
+        $this->books[] = \App\Book::find($book['id']);
     }
 
     public function render()
     {
-        $library = $this->library;
-
-        $library->books->transform(function ($book) {
+        $this->books->transform(function ($book) {
             $book->in_library = true;
 
             return $book;
