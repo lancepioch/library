cbeda431c66d62550114627487fcd3fc13c0284f
Lance Pioch <git@lance.sh>
2020-02-13 01:19:43 -0500

    Add links to libraries

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Libraries</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                @forelse(auth()->user()->libraries as $library)
+                    <li><a href="{{ route('library', [$library]) }}">{{ $library->name }}</a></li>
+                @empty
+                    You don't have any libraries!
+                @endforelse
                 </div>
             </div>
         </div>
