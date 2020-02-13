6da4333a5053f1a6bff196fcc8824eeabb003dfa
Lance Pioch <git@lance.sh>
2020-02-13 01:18:54 -0500

    Increase the size of the search header

--- resources/views/livewire/book-list.blade.php
+++ resources/views/livewire/book-list.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form class="flex w-full max-w-sm mx-auto m-4" wire:submit.prevent="search">
+    <form class="flex w-full max-w-lg mx-auto m-4" wire:submit.prevent="search">
         <input type="search" wire:model.lazy="search" class="flex-1 bg-gray-200 hover:bg-white hover:border-gray-300 focus:outline-none focus:bg-white focus:shadow-outline focus:border-gray-300 appearance-none border border-transparent rounded w-full py-2 px-4 text-gray-700 leading-tight" placeholder="Book Title or ISBN">
 
         <button class="ml-4 flex-shrink-0 bg-blue-500 hover:bg-blue-600 focus:outline-none focus:shadow-outline text-white font-bold py-2 px-4 rounded">Search</button>
